8cc1da8fd627a2a683dab9050c915440429e69e4
Nathan Wong <natdanwongielhan@yahoo.com>
2023-06-21 12:26:38 -0700

    add unfunctional NavBar

--- abacus/src/components/Header.js
+++ abacus/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Header.css";
+import NavBar from "./NavBar";
 
 function Header() {
   return (
     <div className="Header">
-      <div className="Title">Abacus</div>
-      <div className="Name">Name McPlaceHolder</div>
+      <div className="Title">
+        Abacus <div className="Name">Name McPlaceHolder</div>
+      </div>
+      <NavBar />
     </div>
   );
 }
