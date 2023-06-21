06a2f34037b6816e507d39a8037d6c1af76d82de
Nathan Wong <natdanwongielhan@yahoo.com>
2023-06-21 11:11:43 -0700

    style title and add name placeholder

---
+++ abacus/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="Title">Abacus</div>
+      <div className="Name">Name McPlaceHolder</div>
+    </div>
+  );
+}
+
+export default Header;
