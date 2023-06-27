2e8c367cfb386d0923b2cd87c435581908bd683e
Nathan Wong <natdanwongielhan@yahoo.com>
2023-06-27 15:12:48 -0700

    set up Learning Area

--- abacus/src/App.js
+++ abacus/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header.js";
+import LearningArea from "./components/LearningArea.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <LearningArea />
     </div>
   );
 }
---
+++ abacus/src/components/LearningArea.js
@@ -0,0 +1,11 @@
+import "./LearningArea.css";
+
+function LearningArea() {
+  return (
+    <div className="LearningArea">
+      <div>LearningArea</div>
+    </div>
+  );
+}
+
+export default LearningArea;
