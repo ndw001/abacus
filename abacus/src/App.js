60ba40218a5e48d5b13dc6711ec674897cbcace6
Nathan Wong <natdanwongielhan@yahoo.com>
2023-06-27 18:06:56 -0700

    add router/routes

--- abacus/src/App.js
+++ abacus/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Header from "./components/Header.js";
 import LearningArea from "./components/LearningArea.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <LearningArea />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" />
+          <Route exact path="/learning" element={<LearningArea />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
